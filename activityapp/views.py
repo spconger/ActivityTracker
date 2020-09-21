121e43814ba34a6589c20e21ea75ad7005560727
spconger <steve_conger@msn.com>
2020-09-21 14:41:47 -0700

    First view commit

--- activityapp/views.py
+++ activityapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Goal
 
 # Create your views here.
+def index(request):
+    goal_list=Goal.objects.all()
+    return render(request, 'activityapp/index.html', {'goal_list': goal_list})
