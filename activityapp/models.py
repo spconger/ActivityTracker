6b2f59d5fd3a0a39e0ec3f8bfa94a5c1b7be1376
spconger <steve_conger@msn.com>
2020-09-20 21:13:14 -0700

    Partial model commit

--- activityapp/models.py
+++ activityapp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+#Goals
+class Goal(models.Model):
+    goaltext=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.goaltext
+
+    class Meta:
+        db_table='goal'
+
+#categories
+class Category(models.Model):
+    categoryname=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.categoryname
+
+    class Meta:
+        db_table='category'
+        verbose_name_plural='categories'
+
+#projects
+
+class Project(models.Model):
+    projectname=models.CharField(max_length=255)
+    goal=models.ForeignKey(Goal, on_delete=models.DO_NOTHING)
+    category=models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    projectstartdate=models.DateField()
+    projectenddate=models.DateField(null=True, blank=True)
+    projectdescription=models.TextField()
+
+    def __str__(self):
+        return self.projectname
+
+    def timInDays(self):
+        return (self.projectenddate-self.projectstartdate).days
+
+    class Meta:
+        db_table='project'
+
+#milestones
+
+
+#activities
+
+#notes
--- activityapp/tests.py
+++ activityapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from datetime import date
+from .models import Goal, Category, Project
 
-# Create your tests here.
+class ProjectTest(TestCase):
+    def setUp(self):
+        g=Goal(goaltext='Some Goal')
+        c=Category(categoryname='testing')
+        start=date(2020,9,18)
+        end=date(2020,9,22)
+        self.p = Project(projectname='project',goal=g,category=c, projectstartdate=start, projectenddate=end, projectdescription='some project')
+    
+    def test_date_function(self):
+        self.assertTrue(p.timeInDays(), 4)
+
+           
